bf9a28ec08ea62aa9bbcf71e6ba383fa44e0154e
austinbowers <austin.bowers75@gmail.com>
2023-03-18 14:08:17 -0500

    Mobile view for Members list.

--- resources/views/pages/members/index.blade.php
+++ resources/views/pages/members/index.blade.php
@@ -3,7 +3,7 @@
                 <div class="mx-auto max-w-3xl">
                     <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl border-b pb-5 mb-5">Members
             </h1>
-                    <div class="columns-3 space-y-3">
+                    <div class="columns-1 md:columns-2 lg:columns-3 space-y-3">
                         
                         @foreach ($users as $user)
                             <a class="block text-red-500 hover:text-red-900 transition duration-200" href="/members/{{ $user->id }}">
